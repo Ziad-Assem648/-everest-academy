b07a4f067fff435d677fba2f3430a62eac466ca7
Ziad-Assem648 <ahmdzyadasm@gmail.com>
2026-07-29 21:55:14 +0300

    Replace navbar logo with new logo-navbar.png

--- user/src/components/AppNavbar.jsx
+++ user/src/components/AppNavbar.jsx
@@ -86,7 +86,7 @@ export default function AppNavbar() {
           boxShadow: c.shadow, direction: dir,
         }}>
           <Link to="/home" style={{ display: "flex", alignItems: "center", gap: 8, textDecoration: "none" }}>
-            <img src="/image/new_logo-removebg-preview.png" alt="Logo" style={{ height: 52, objectFit:"contain" }} />
+            <img src="/image/logo-navbar.png" alt="Logo" style={{ height: 52, objectFit:"contain" }} />
           </Link>
           <div style={{ display: "flex", alignItems: "center", gap: 8 }}>
             {user && <NotificationBell userId={user.id} />}
@@ -117,7 +117,7 @@ export default function AppNavbar() {
         boxShadow: c.shadow, direction: dir
       }}>
         <Link to="/home" style={{ display: "flex", alignItems: "center", gap: 10, textDecoration: "none" }}>
-          <img src="/image/new_logo-removebg-preview.png" alt="Logo" style={{ height: 80, objectFit:"contain" }} />
+          <img src="/image/logo-navbar.png" alt="Logo" style={{ height: 80, objectFit:"contain" }} />
         </Link>
 
         <nav style={{ display: "flex", alignItems: "center", gap: 0 }}>
